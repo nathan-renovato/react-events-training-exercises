d2716da6cd0779eae5a5676bd979d0797acbcbf1
unknown <nathan.renovato300@gmail.com>
2022-09-19 16:12:55 -0300

    Resolution exercise 03

--- src/App3.js
+++ src/App3.js
@@ -1,12 +1,44 @@
 import './App.css';
+import { useRef } from 'react';
 
 function App3() {
+  const danielRef = useRef(null);
+  const joaoRef = useRef(null);
+  const pedroRef = useRef(null);
+
+  function handleClickDaniel() {
+    danielRef.current.style.fontSize = '30px';
+    danielRef.current.style.color = 'red';
+  }
+  function handleClickJoao() {
+    joaoRef.current.style.fontSize = '60px';
+    joaoRef.current.style.color = 'green';
+  }
+  function handleClickPedro() {
+    pedroRef.current.style.fontSize = '90px';
+    pedroRef.current.style.color = 'blue';
+  }
 
   return (
     <div className="App">
-      <strong>Daniel</strong>
-      <strong>João</strong>
-      <strong>Pedro</strong>
+      <strong
+        ref={danielRef}
+        onClick={() => handleClickDaniel()}
+      >
+        Daniel
+      </strong>
+      <strong
+        ref={joaoRef}
+        onClick={() => handleClickJoao()}
+      >
+        João
+      </strong>
+      <strong
+        ref={pedroRef}
+        onClick={() => handleClickPedro()}
+      >
+        Pedro
+      </strong>
     </div>
   );
 }
